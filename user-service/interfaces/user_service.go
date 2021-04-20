6c81655fb7f6d7d5bce459eb868ce845b11014ef
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-20 15:47:45 +0300

    Refactor repositories interfaces

--- user-service/interfaces/user_service.go
+++ user-service/interfaces/user_service.go
@@ -7,15 +7,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type UserRepository interface {
-	FindByID(ctx context.Context, userID primitive.ObjectID) (model.User, error)
-	FindByLogin(ctx context.Context, userLogin string) (model.User, error)
-	FindAll(ctx context.Context, page int) ([]model.User, error)
-	Create(ctx context.Context, user model.User) (model.User, error)
-	Update(ctx context.Context, user model.User) error
-	Delete(ctx context.Context, userID primitive.ObjectID) (model.User, error)
-}
-
 type UserService interface {
 	FindByID(ctx context.Context, userID primitive.ObjectID) (model.User, error)
 	FindByLogin(ctx context.Context, userLogin string) (model.User, error)
