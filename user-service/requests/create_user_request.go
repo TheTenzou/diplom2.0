6018ec9da616814a53d95ad0fc3122fbee340b8c
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-22 10:48:59 +0300

    Update requests

--- user-service/requests/create_user_request.go
+++ user-service/requests/create_user_request.go
@@ -1,7 +1,7 @@
 package requests
 
 type CreateUserRequest struct {
-	Login    string `json:"login" binding:"required"`
+	Login    string `json:"login" binding:"required,gte=6,lte=32"`
 	Password string `json:"password" binding:"required,gte=6,lte=30"`
-	Name     string `json:"name" binding:"omitempty"`
+	Name     string `json:"name" binding:"omitempty,lte=50"`
 }
--- user-service/requests/update_user_request.go
+++ user-service/requests/update_user_request.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UpdateUserRequest struct {
 	ID       primitive.ObjectID `json:"id" binding:"required"`
-	Login    string             `json:"login" binding:"required"`
-	Password string             `json:"password" binding:"required,gte=6,lte=30"`
-	Name     string             `json:"name" binding:"omitempty"`
+	Login    string             `json:"login" binding:"omitempty,gte=6,lte=32"`
+	Password string             `json:"password" binding:"omitempty,gte=6,lte=30"`
+	Name     string             `json:"name" binding:"omitempty,lte=50"`
 }
