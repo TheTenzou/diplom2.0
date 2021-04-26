e64d27b240cf978869d4706326c452c6aa947edc
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-26 17:01:33 +0300

    Add tests for deleting user

--- user-service/handler/user_handler_test.go
+++ user-service/handler/user_handler_test.go
@@ -355,3 +355,69 @@ func TestUpdateUser(test *testing.T) {
 		mockUserService.AssertCalled(test, "Update", updateArgs...)
 	})
 }
+
+func TestDeleteUser(test *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	test.Run("Success", func(test *testing.T) {
+		userID, _ := primitive.ObjectIDFromHex("607db993fad7324170a4debc")
+
+		mockUserResponse := model.User{
+			ID:     userID,
+			Login:  "alice",
+			Name:   "Alice",
+			Status: "deleted",
+		}
+
+		mockUserService := new(mocks.MockUserService)
+		mockUserService.On("Delete", mock.AnythingOfType("*context.emptyCtx"), userID).Return(mockUserResponse, nil)
+
+		recorder := httptest.NewRecorder()
+
+		router := gin.Default()
+
+		InitUserHandler(router, mockUserService)
+
+		request, err := http.NewRequest(http.MethodDelete, "/api/users/v1/user/"+userID.Hex(), nil)
+
+		assert.NoError(test, err)
+
+		router.ServeHTTP(recorder, request)
+
+		responseBody, err := json.Marshal(mockUserResponse)
+
+		assert.NoError(test, err)
+
+		assert.Equal(test, http.StatusOK, recorder.Code)
+		assert.Equal(test, responseBody, recorder.Body.Bytes())
+		mockUserService.AssertExpectations(test)
+	})
+
+	test.Run("NotFound", func(test *testing.T) {
+
+		userID, _ := primitive.ObjectIDFromHex("607db993fad7324170a4debc")
+
+		mockErr := apperrors.NewNotFound("user", userID.Hex())
+		mockUserService := new(mocks.MockUserService)
+		mockUserService.On("Delete", mock.Anything, userID).Return(model.User{}, mockErr)
+
+		recorder := httptest.NewRecorder()
+
+		router := gin.Default()
+
+		InitUserHandler(router, mockUserService)
+
+		request, err := http.NewRequest(http.MethodDelete, "/api/users/v1/user/"+userID.Hex(), nil)
+		assert.NoError(test, err)
+
+		router.ServeHTTP(recorder, request)
+
+		respBody, err := json.Marshal(mockErr)
+
+		assert.NoError(test, err)
+
+		assert.Equal(test, mockErr.StatusCode, recorder.Code)
+		assert.Equal(test, respBody, recorder.Body.Bytes())
+		mockUserService.AssertExpectations(test)
+	})
+}
