8ecd7cae364babeb01c440b23d2053edbf94e93a
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-28 13:52:39 +0300

    Add tsodd service

---
+++ tsodd-service/src/main/java/ru/thetenzou/tsoddservice/TsoddServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.thetenzou.tsoddservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsoddServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsoddServiceApplication.class, args);
+	}
+
+}
