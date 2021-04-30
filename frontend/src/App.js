29fcce3ee9393c9dca514360062151127442a700
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-30 16:27:16 +0300

    Add request to back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [userService, setUserService] = useState(null);
+
+  const [tsoddService, setTsoddService] = useState(null);
+
+  useEffect(() => {
+    axios.get("/api/users/health").then((response) => {
+      setUserService(response.data.status);
+    });
+  }, [setUserService]);
+
+  useEffect(() => {
+    axios.get("/api/tsodd/health").then((response) => {
+      setTsoddService(response.data.status);
+    });
+  }, [setTsoddService]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          user service is {userService ? userService : "offline"} <br />
+          tsodd service is {tsoddService ? tsoddService : "offline"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
