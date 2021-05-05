41046543030ebed9635e75999c638f0f27e89518
betomex <thebetomex@gmail.com>
2021-05-05 19:44:54 +0300

    merged with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,80 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import MainNavigation from "./components/MainNavigation";
+import MyMap from "./components/Map";
+import Info from "./components/Info";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
+import { BDiv } from 'bootstrap-4-react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/table1">
+            <TableTSOD />
+          </Route>
+          <Route path="/table2">
+            <TableFlows />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return (
+    <div className="wrapper">
+      <MainNavigation />
+      <BDiv mx="auto" style={{ width: '80%', height: '80%', padding: '10px', backgroundColor: 'rgba(86, 61, 124, .5)' }}>
+        <MyMap />
+      </BDiv>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="wrapper">
+      <MainNavigation />
+      <BDiv mx="auto" style={{ width: '80%', height: '80%', padding: '10px', backgroundColor: 'rgba(176, 167, 199, .4)' }}>
+        <Info />
+      </BDiv>
+    </div>
+  );
+}
+
+function TableTSOD() {
+  return (
+    <div className="wrapper">
+      <MainNavigation />
+        <BDiv mx="auto" style={{ width: '80%', height: '80%', padding: '10px', backgroundColor: 'rgba(176, 167, 199, .4)' }}>
+          Табличка с ТСОД
+        </BDiv>
+    </div>
+  );
+}
+
+function TableFlows() {
+  return (
+    <div className="wrapper">
+      <MainNavigation />
+        <BDiv mx="auto" style={{ width: '80%', height: '80%', padding: '10px', backgroundColor: 'rgba(176, 167, 199, .4)' }}>
+          Табличка с Потоками
+        </BDiv>
+    </div>
+  );
+}
+
+/*function App() {
   const [userService, setUserService] = useState(null);
 
   const [tsoddService, setTsoddService] = useState(null);
@@ -21,16 +92,9 @@ function App() {
   }, [setTsoddService]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <p>
           user service is {userService ? userService : "offline"} <br />
           tsodd service is {tsoddService ? tsoddService : "offline"}
-        </p>
-      </header>
-    </div>
+    </p>
   );
-}
-
-export default App;
+}*/
---
+++ frontend/src/components/MainNavigation.js
@@ -0,0 +1,66 @@
+import MySidebar from './Sidebar';
+import React, { Component } from 'react';
+import { Navbar, Nav, Button, Form, Collapse, BImg } from 'bootstrap-4-react';
+import Sidebar from "react-sidebar";
+
+export default class MainNavigation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(e, open) {
+    if (open) {
+      e.preventDefault();
+    }
+    this.setState({ sidebarOpen: open });
+  }
+
+  render() {
+    const bootstrap_icon = 'https://static.tildacdn.com/tild3236-6566-4134-b464-623730343364/square-md-a.png';
+    return (
+      <React.Fragment>
+        <Sidebar
+          sidebar={<MySidebar />}
+          open={this.state.sidebarOpen}
+          pullRight={true}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: "white", zIndex: "1001", width: "300px"} }}
+        >
+        </Sidebar>
+
+        <Navbar expand="lg" dark bg="dark" mb="3">
+          <Navbar.Brand href="/">
+            <BImg
+              src={bootstrap_icon}
+              width="30"
+              height="30"
+              display="inline-block"
+              align="top"
+              mr="1"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggler target="#navbar" />
+          <Collapse navbar id="navbar">
+            <Navbar.Nav mr="auto">
+              <Nav.ItemLink href="/" active>Главная</Nav.ItemLink>
+              <Nav.ItemLink href="/table1">Таблица №1</Nav.ItemLink>
+              <Nav.ItemLink href="/table2">Таблица №2</Nav.ItemLink>
+              <Nav.ItemLink href="/about">Дополнительная информация</Nav.ItemLink>
+              <div id="modalRoot"></div>
+            </Navbar.Nav>
+            <Form inline my="2 lg-0">
+              <Button outline light mr="sm-2" my="2 sm-2" onClick={(e) => this.onSetSidebarOpen(e, true)}>Сортировки</Button>
+              <Form.Input type="search" placeholder="Search" mr="sm-2" />
+              <Button outline info my="2 sm-0">Search</Button>
+            </Form>
+          </Collapse>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
+}
