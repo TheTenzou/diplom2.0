dfe57be6080abba33f892953191da1359285539f
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-09 20:55:17 +0300

    Fix ScheduleRepository.kt

--- tsodd-service/src/main/kotlin/ru/thetenzou/tsoddservice/repository/schedule/ScheduleRepository.kt
+++ tsodd-service/src/main/kotlin/ru/thetenzou/tsoddservice/repository/schedule/ScheduleRepository.kt
@@ -2,6 +2,7 @@ package ru.thetenzou.tsoddservice.repository.schedule
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import ru.thetenzou.tsoddservice.model.schedule.Schedule
 
 @RepositoryRestResource
-interface ScheduleRepository : JpaRepository<ScheduleRepository, Long>
+interface ScheduleRepository : JpaRepository<Schedule, Long>
