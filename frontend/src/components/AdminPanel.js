30d18d3decf7675192d87600c0166011448d1cd9
betomex <thebetomex@gmail.com>
2021-05-11 13:36:19 +0300

    create and delete users

--- frontend/src/components/AdminPanel.js
+++ frontend/src/components/AdminPanel.js
@@ -2,6 +2,7 @@ import '../AdminPanel.css';
 import React, { Component, useState, useEffect } from "react";
 import { BDiv, Card, Button, Modal, Form } from 'bootstrap-4-react';
 import axios from "axios";
+import { Redirect } from 'react-router';
 
 export default class AdminPanel extends Component {
   render() {
@@ -10,7 +11,10 @@ export default class AdminPanel extends Component {
         <div style={{ height: '15%' }}></div>
         <BDiv m="auto" className="mainField">
           <BDiv m="auto" className="contentField">
-            <h1 className="title">Список пользователей</h1>
+            <h1 style={{ paddingBottom: '10px', borderBottom: '1px solid gray' }}>Список пользователей</h1>
+            <CreateModal />
+            <Button dark m="2" onClick={() => window.location.reload()}>Обновить список</Button>
+            <p style={{ margin: '5px', borderBottom: '1px solid gray' }}></p>
             <UsersData />
             <UserCards />
           </BDiv>
@@ -39,7 +43,7 @@ const UserCards = () => {
         </Card.Body>
         <Card.Footer>
           <UpdateModal data={data[i]} />
-          <Button primary mx="2">Удалить</Button>
+          <DeleteModal data={data[i]}/>
         </Card.Footer>
       </Card>
     );
@@ -75,22 +79,6 @@ const UpdateModal = (data) => {
   const [adminRole, setAdminRole] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
   const [goodMessage, setGoodMessage] = useState(null);
-
-  useEffect(() => {
-    setLogin(login);
-  }, [login]);
-
-  useEffect(() => {
-    setName(name);
-  }, [name]);
-
-  useEffect(() => {
-    setErrorMessage(errorMessage);
-  }, [errorMessage]);
-
-  useEffect(() => {
-    setGoodMessage(goodMessage);
-  }, [goodMessage]);
 
   return (
     <>
@@ -154,6 +142,144 @@ const UpdateModal = (data) => {
                   console.log(er.response);
                 });
               }}>Обновить</Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal.Dialog>
+      </Modal>
+    </>
+  );
+}
+
+const DeleteModal = (data) => {
+  const id = data["data"]["id"];
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [goodMessage, setGoodMessage] = useState(null);
+
+  return (
+    <>
+      <Button primary data-toggle="modal" data-target={"#delete" + id}>Удалить</Button>
+      <Modal id={"delete" + id} fade>
+        <Modal.Dialog centered>
+          <Modal.Content>
+            <Modal.Header>
+              <Modal.Title>Точно удалить?</Modal.Title>
+              <Modal.Close>
+                <span aria-hidden="true">&times;</span>
+              </Modal.Close>
+            </Modal.Header>
+            <Modal.Body>
+              {errorMessage? errorMessage : ''}
+              {goodMessage? goodMessage : ''}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button secondary data-dismiss="modal">Закрыть</Button>
+              <Button primary onClick={(e) => {
+                e.preventDefault();
+
+                var myUrl = 'api/users/v1/user/' + id;
+                console.log(myUrl);
+                
+                var args = {
+                  method: 'delete',
+                  url: myUrl,
+                  headers: {
+                    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+                  }
+                };
+
+                axios(args).then((r) => {
+                  if (r.statusText === "OK") {
+                    setGoodMessage("Пользователь успешно удалён!");
+                  }
+                }).catch((er) => {
+                  setErrorMessage(er.response.data.error.message);
+                  console.log(er.response);
+                });
+              }}>Удалить</Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal.Dialog>
+      </Modal>
+    </>
+  )
+}
+
+const CreateModal = () => {
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [userRole, setUserRole] = useState(false);
+  const [adminRole, setAdminRole] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [goodMessage, setGoodMessage] = useState(null);
+
+  return (
+    <>
+      <Button success m="2" data-toggle="modal" data-target="#createUser">Создать пользователя</Button>
+      <Modal id="createUser" fade>
+        <Modal.Dialog>
+          <Modal.Content>
+            <Modal.Header>
+              <Modal.Title>Новые данные пользователя:</Modal.Title>
+              <Modal.Close>
+                <span aria-hidden="true">&times;</span>
+              </Modal.Close>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Input type="login" id="loginInput" placeholder="Login" my="2" onChange={(e) => setLogin(e.target.value)} />
+                <Form.Input type="password" id="passwordInput" placeholder="Password" my="2" onChange={(e) => setPassword(e.target.value)} />
+                <Form.Input id="nameInput" placeholder="Name" my="2" onChange={(e) => setName(e.target.value)} />
+                <Form.Text style={{ color: 'red' }}>{errorMessage ? errorMessage : ''}</Form.Text>
+                <Form.Group>
+                  <Form.Check>
+                    <Form.Checkbox id="roleUser" value="ROLE_USER" onChange={(e) => setUserRole(e.target.value)} />
+                    <Form.CheckLabel htmlFor="roleUser">ROLE_USER</Form.CheckLabel>
+                  </Form.Check>
+                  <Form.Check>
+                    <Form.Checkbox id="roleAdmin" value="ROLE_ADMIN" onChange={(e) => setAdminRole(e.target.value)} />
+                    <Form.CheckLabel htmlFor="roleAdmin">ROLE_ADMIN</Form.CheckLabel>
+                  </Form.Check>
+                </Form.Group>
+                <Form.Text style={{ color: 'green' }}>{goodMessage ? goodMessage : ''}</Form.Text>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button secondary data-dismiss="modal">Закрыть</Button>
+              <Button primary onClick={(e) => {
+                e.preventDefault();
+
+                var role = []
+                if (adminRole) role.push(adminRole);
+                if (userRole) role.push(userRole);
+
+                console.log(login, password, name, role);
+                
+                var args = {
+                  method: 'put',
+                  url: 'api/users/v1/user',
+                  headers: {
+                    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+                  },
+                  data: {
+                    "login": login,
+                    "password": password,
+                    "name": name,
+                    "role": role
+                  }
+                };
+
+                axios(args).then((r) => {
+                  if (r.status === 201) {
+                    setGoodMessage("Пользователь успешно создан!");
+                  }
+                  console.log(r);
+                }).catch((er) => {
+                  setErrorMessage(er.response.data.message);
+                });
+              }}>Создать</Button>
             </Modal.Footer>
           </Modal.Content>
         </Modal.Dialog>
