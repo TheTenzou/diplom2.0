baa7ac9e521174c41215433a0b654cddeb293c67
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-24 18:39:34 +0300

    Add some comments

--- tsodd-service/src/main/kotlin/ru/thetenzou/tsoddservice/schedule/model/ScheduledTask.kt
+++ tsodd-service/src/main/kotlin/ru/thetenzou/tsoddservice/schedule/model/ScheduledTask.kt
@@ -6,29 +6,57 @@ import ru.thetenzou.tsoddservice.tsodd.model.Tsodd
 import java.time.LocalDate
 import javax.persistence.*
 
+/**
+ * A ScheduledTask is model that represent scheduled task record from database
+ *
+ * @param id is id of the record
+ * @param schedule schedule that this task belongs
+ * @param date when this tasks will be or was performed
+ * @param tsodd target of the task
+ * @param taskType type of the task
+ * @param crew that will perform the task
+ */
 @Entity
 @Table(name = "scheduled_tasks")
 data class ScheduledTask(
 
+    /**
+     * is id of the record
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long?,
 
+    /**
+     * schedule that this task belongs
+     */
     @ManyToOne
     @JoinColumn(name = "schedule_id")
     var schedule: Schedule?,
 
+    /**
+     * date when this tasks will be or was performed
+     */
     @Column(name = "date")
     var date: LocalDate?,
 
+    /**
+     * target of the task
+     */
     @ManyToOne
     @JoinColumn(name = "tsodd_id")
     var tsodd: Tsodd,
 
+    /**
+     * type of the task
+     */
     @ManyToOne
     @JoinColumn(name = "task_type_id")
     var taskType: TaskType,
 
+    /**
+     * crew that will perform the task
+     */
     @ManyToOne
     @JoinColumn(name = "crew_id")
     var crew: Crew,
