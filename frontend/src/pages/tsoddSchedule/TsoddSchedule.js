87c3b4af54d27ec490f6ff99928f60626cf503d8
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-30 13:10:38 +0300

    Add tsodd info

--- frontend/src/pages/tsoddSchedule/TsoddSchedule.js
+++ frontend/src/pages/tsoddSchedule/TsoddSchedule.js
@@ -3,35 +3,41 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BDiv } from "bootstrap-4-react";
 
-import ScheduleTable from "./components/ScheduleTable"
+import ScheduleTable from "./components/ScheduleTable";
 
 function TsoddSchedule() {
   let { id } = useParams();
 
   const [schedule, setSchedule] = useState(null);
+  const [scheduleInfo, setScheduleInfo] = useState(null);
 
   useEffect(() => {
     axios.get(`/api/tsodd/v1/schedule/${id}?size=240`).then((response) => {
-      let scheduleResponse = response.data
+      let scheduleResponse = response.data;
+      
+      setScheduleInfo({
+        name : scheduleResponse.name,
+        createdDate : scheduleResponse.createdDate,
+        startDate : scheduleResponse.startDate,
+        endDate : scheduleResponse.endDate
+      })
 
       let schedule = new Map();
       scheduleResponse.tasks.data.forEach((task) => {
-
         if (!schedule.has(task.date)) {
-          schedule.set(task.date, new Map())
+          schedule.set(task.date, new Map());
         }
         if (!schedule.get(task.date).has(task.crew.id)) {
-          schedule.get(task.date).set(task.crew.id, [])
+          schedule.get(task.date).set(task.crew.id, []);
         }
         schedule.get(task.date).get(task.crew.id).push({
           taskType: task.taskType,
-          tsodd: task.tsodd
-        })
-
-      })
+          tsodd: task.tsodd,
+        });
+      });
       setSchedule(schedule);
     });
-  }, [setSchedule, id]);
+  }, [setSchedule, id , setScheduleInfo]);
 
   const [crewList, setCrewList] = useState(null);
 
@@ -43,9 +49,12 @@ function TsoddSchedule() {
 
   return (
     <BDiv mx="auto" className="tsoddSchedule">
-      Schedule
-      <br />
-      <ScheduleTable crew={crewList} schedule={schedule}/>
+      <h3>План обслуживания ТСОДД</h3>
+      Название: {scheduleInfo?.name} <br/>
+      Дата создания: {scheduleInfo?.createdDate} <br/>
+      Дата начала: {scheduleInfo?.startDate} <br/>
+      Дата окончания: {scheduleInfo?.endDate} <br/>
+      <ScheduleTable crew={crewList} schedule={schedule} />
     </BDiv>
   );
 }
