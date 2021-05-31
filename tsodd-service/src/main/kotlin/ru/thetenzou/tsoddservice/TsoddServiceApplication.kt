32802ffeaeaade047af28b01688559b6216c4ad1
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-31 22:25:14 +0300

    Update test data

--- tsodd-service/src/main/kotlin/ru/thetenzou/tsoddservice/TsoddServiceApplication.kt
+++ tsodd-service/src/main/kotlin/ru/thetenzou/tsoddservice/TsoddServiceApplication.kt
@@ -39,13 +39,14 @@ class TsoddServiceApplication(
      */
     fun initdb() = CommandLineRunner {
 
-        val firstTsoddGroup = TsoddGroup(id = 0L, "first group", taskGroup = null)
-        val secondTsoddGroup = TsoddGroup(id = 0L, "first group", taskGroup = null)
+        val firstTsoddGroup = TsoddGroup(id = 0L, "дорожные светофоры", taskGroup = null)
+        val secondTsoddGroup = TsoddGroup(id = 0L, "дорожные знаки", taskGroup = null)
+        val thirdTsoddGroup = TsoddGroup(id = 0L, "дорожная разметка", taskGroup = null)
 
-        val firstTsoddType = TsoddType(id = 0L, tsoddGroup = firstTsoddGroup, name = "first tsodd type")
-        val secondTsoddType = TsoddType(id = 0L, tsoddGroup = secondTsoddGroup, name = "second tsodd type")
+        val firstTsoddType = TsoddType(id = 0L, tsoddGroup = firstTsoddGroup, name = "2.1")
+        val secondTsoddType = TsoddType(id = 0L, tsoddGroup = secondTsoddGroup, name = "1.3")
 
-        val tsoddCondition = TsoddCondition(id = 0L, name = "condition")
+        val tsoddCondition = TsoddCondition(id = 0L, name = "хорошее")
 
         val tsoddList = listOf(
             Tsodd(
@@ -57,8 +58,9 @@ class TsoddServiceApplication(
                     arrayOf(
                         GeometryFactory().createPoint(Coordinate(1.0, 1.0)),
                         GeometryFactory().createPoint(Coordinate(1.0, 2.0)),
-                    )),
-                positionDescription = "placed some where"
+                    )
+                ),
+                positionDescription = "ул. Красная 27"
             ),
             Tsodd(
                 id = 0L,
@@ -68,69 +70,72 @@ class TsoddServiceApplication(
                 coordinates = GeometryFactory().createGeometryCollection(
                     arrayOf(
                         GeometryFactory().createPoint(Coordinate(3.0, 3.0)),
-                    )),
-                positionDescription = "placed some where"
+                    )
+                ),
+                positionDescription = "ул. Красная 17"
             ),
         )
 
         val firstTaskGroup =
-            TaskGroup(id = 0L, name = "first group", tsoddGroups = listOf(firstTsoddGroup), taskType = emptyList())
+            TaskGroup(
+                id = 0L,
+                name = "Обслуживание светофоров",
+                tsoddGroups = listOf(firstTsoddGroup),
+                taskType = emptyList()
+            )
         val secondTaskGroup =
-            TaskGroup(id = 0L, name = "first group", tsoddGroups = listOf(secondTsoddGroup), taskType = emptyList())
+            TaskGroup(
+                id = 0L,
+                name = "Обслуживание знаков",
+                tsoddGroups = listOf(secondTsoddGroup),
+                taskType = emptyList()
+            )
 
         val taskTypeList = listOf(
             TaskType(
                 id = 0L,
-                name = "first task type",
+                name = "Ревизия светофора",
                 taskGroup = firstTaskGroup,
-                timeIntervalInDays = 10,
+                timeIntervalInDays = 30,
+                durationHours = 1,
+                moneyResources = 2.0,
+                effectiveness = 4,
+            ),
+            TaskType(
+                id = 0L,
+                name = "Осмотр кабельных трасс и мест подключений",
+                taskGroup = firstTaskGroup,
+                timeIntervalInDays = 20,
                 durationHours = 2,
-                moneyResources = 5.0,
-                effectiveness = 2,
-            ),
-            TaskType(
-                id = 0L,
-                name = "second task type",
-                taskGroup = firstTaskGroup,
-                timeIntervalInDays = 5,
-                durationHours = 3,
                 moneyResources = 4.0,
-                effectiveness = 1,
+                effectiveness = 3,
             ),
             TaskType(
                 id = 0L,
-                name = "third task type",
+                name = "Техническое обслуживание светофора",
                 taskGroup = firstTaskGroup,
-                timeIntervalInDays = 5,
-                durationHours = 3,
+                timeIntervalInDays = 60,
+                durationHours = 5,
                 moneyResources = 4.0,
-                effectiveness = 1,
+                effectiveness = 7,
             ),
             TaskType(
                 id = 0L,
-                name = "forth task type",
+                name = "Выявление неисправностей на предмет механических повреждений",
                 taskGroup = secondTaskGroup,
-                timeIntervalInDays = 5,
-                durationHours = 3,
-                moneyResources = 4.0,
-                effectiveness = 1,
+                timeIntervalInDays = 30,
+                durationHours = 1,
+                moneyResources = 1.0,
+                effectiveness = 2,
             ),
         )
 
         val crewList = listOf(
-            Crew(
-                id = 0L,
-                name = "first crew"
-            ),
-            Crew(
-                id = 0L,
-                name = "second crew"
-            ),
-            Crew(
-                id = 0L,
-                name = "third crew"
-            ),
+            Crew(id = 0L, name = "Бригада 1"),
+            Crew(id = 0L, name = "Бригада 2"),
+            Crew(id = 0L, name = "Бригада 3"),
         )
+
         firstTsoddGroup.taskGroup = listOf(firstTaskGroup)
         secondTsoddGroup.taskGroup = listOf(secondTaskGroup)
 
