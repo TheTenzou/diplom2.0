ace310533243fa54faade12c6d7e292c6f6d8de5
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-31 22:31:27 +0300

    Improve table style

--- frontend/src/pages/tsoddSchedule/components/ScheduleTable.js
+++ frontend/src/pages/tsoddSchedule/components/ScheduleTable.js
@@ -4,10 +4,14 @@ import { Badge } from "bootstrap-4-react";
 import "../css/table.css";
 
 function ScheduleTable({ crew, schedule }) {
+  let col = [<col />];
+  crew?.forEach((crew) => {
+    col.push(<col width="20px" />);
+  });
   let firstRow = [<th></th>];
 
   crew?.forEach((crew) => {
-    firstRow.push(<th>{crew.name}</th>);
+    firstRow.push(<th style={{ textAlign: "center" }}>{crew.name}</th>);
   });
 
   let table = [
@@ -24,20 +28,14 @@ function ScheduleTable({ crew, schedule }) {
     days.forEach((day) => {
       const crewsTasks = schedule.get(day);
 
-      let dayTaskList = [<th>{day}</th>];
+      let dayTaskList = [<th style={{ minWidth: "88px" }}>{day}</th>];
       crew?.forEach((crew) => {
         let crewTasks = [];
         if (crewsTasks.has(crew.id)) {
           crewsTasks.get(crew.id).forEach((task) => {
             crewTasks.push(
               <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
+                style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", }}>
                 <div>
                   Задача: {task.taskType.name}
                   <br />
@@ -46,16 +44,9 @@ function ScheduleTable({ crew, schedule }) {
                   Местоположение: {task.tsodd.positionDescription}
                 </div>
 
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Badge info className="btn btn-outline-light" style={{marginBottom: ".2rem"}}>&#127760;</Badge>
-                  <Badge primary className="btn btn-outline-light" style={{marginBottom: ".2rem"}}>&#9998;</Badge>
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                  <Badge info className="btn btn-outline-light" style={{ marginBottom: ".2rem" }}>&#127760;</Badge>
+                  <Badge primary className="btn btn-outline-light" style={{ marginBottom: ".2rem" }}>&#9998;</Badge>
                   <Badge danger className="btn btn-outline-light">&#10008;</Badge>
                 </div>
               </div>
