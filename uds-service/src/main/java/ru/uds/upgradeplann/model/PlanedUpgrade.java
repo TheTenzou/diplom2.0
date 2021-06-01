ce9227db73a82c75253f72a2e11e8fff565df76c
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-06-01 19:40:20 +0300

    Add Upgrade plan model

---
+++ uds-service/src/main/java/ru/uds/upgradeplann/model/PlanedUpgrade.java
@@ -0,0 +1,25 @@
+package ru.uds.upgradeplann.model;
+
+import lombok.Data;
+import ru.uds.flow.model.Flow;
+import ru.uds.udsupgradeevents.model.UpgradeEvent;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "planned_upgrade")
+public class PlanedUpgrade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany
+    @Column(name = "flow_id")
+    private Flow flow;
+
+    @OneToMany
+    @Column(name = "upgrade_event_id")
+    private UpgradeEvent upgradeEvent;
+}
---
+++ uds-service/src/main/java/ru/uds/upgradeplann/model/UpgradePlan.java
@@ -0,0 +1,25 @@
+package ru.uds.upgradeplann.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "upgrade_plan")
+public class UpgradePlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "resource_limit")
+    private Double resourceLimit;
+}
