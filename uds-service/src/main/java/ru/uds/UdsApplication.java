0b1a6132f10dc623c5d4a7a3b6b032fac360f2b7
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-06-01 17:13:10 +0300

    Add uds service

---
+++ uds-service/src/main/java/ru/uds/UdsApplication.java
@@ -0,0 +1,13 @@
+package ru.uds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdsApplication.class, args);
+	}
+
+}
---
+++ uds-service/src/main/java/ru/uds/controller/TestController.java
@@ -0,0 +1,15 @@
+package ru.uds.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/uds/v1")
+public class TestController {
+
+    @GetMapping
+    public String hello() {
+        return "hello world";
+    }
+}
