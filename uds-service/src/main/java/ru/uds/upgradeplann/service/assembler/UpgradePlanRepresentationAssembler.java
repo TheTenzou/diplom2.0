4d33cd4ea6042bab0d7c7bfde010d5dda8e70f77
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-06-02 09:22:58 +0300

    Update links

--- uds-service/src/main/java/ru/uds/upgradeplann/service/assembler/UpgradePlanRepresentationAssembler.java
+++ uds-service/src/main/java/ru/uds/upgradeplann/service/assembler/UpgradePlanRepresentationAssembler.java
@@ -5,17 +5,19 @@ import org.springframework.stereotype.Component;
 import ru.uds.upgradeplann.controller.UpgradePlanControllerV2;
 import ru.uds.upgradeplann.dto.response.UpgradePlanRepresentation;
 import ru.uds.upgradeplann.model.UpgradePlan;
+import ru.uds.upgradeplann.repository.UpgradePlanRepository;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class UpgradePlanRepresentationAssembler implements RepresentationModelAssembler<UpgradePlan, UpgradePlanRepresentation> {
 
     @Override
     public UpgradePlanRepresentation toModel(UpgradePlan entity) {
-        Class<UpgradePlanControllerV2> controllerClass = UpgradePlanControllerV2.class;
         UpgradePlanRepresentation model = new UpgradePlanRepresentation(entity);
-        model.add(linkTo(controllerClass).slash(entity.getId()).withSelfRel());
+        model.add(linkTo(UpgradePlanControllerV2.class).slash(entity.getId()).withRel("details"));
+        model.add(linkTo(UpgradePlanRepository.class).slash("/api/uds/upgradePlans").slash(entity.getId()).withSelfRel());
         return model;
     }
 }
